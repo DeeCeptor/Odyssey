fc9ec5bb1003f624a6ce93fe164fec343f2d1b74
Squirrell <ksquires11@gmail.com>
2015-09-07 16:30:18 -0800

    added a random chance for events when sailing, only one event right now but will add many more very soon

---
+++ Odyssey/Assets/Scripts/EventManagement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManagement : MonoBehaviour {
+
+	public GameObject[] SeaEventList;
+	
+	public int framesPerCheckRegular = 240;
+	public int framesPerCheck = 240;
+	private int frameIterator = 0;
+	public float eventChance = 25f;
+	public bool paused = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(paused == false)
+			{
+			if(frameIterator>framesPerCheck)
+				{
+				frameIterator = 0;
+				framesPerCheck = framesPerCheckRegular + Random.Range(0,121);
+				if(Random.Range (0,100) <eventChance)
+				{
+				HaveEvent();
+				}
+				
+				}
+			frameIterator = frameIterator + 1;
+			}
+	}
+	
+	public void HaveEvent()
+	{
+		Instantiate(SeaEventList[0]);
+	}
+
+	public void Pause()
+	{
+		paused = true;
+	}
+	
+	public void Unpause()
+	{
+		paused = false;
+	}
+	
+}
--- Odyssey/Assets/Scripts/ResourceManager.cs
+++ Odyssey/Assets/Scripts/ResourceManager.cs
@@ -3,15 +3,20 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ResourceManager : MonoBehaviour {
+//display texts for present amount
 public GameObject waterText;
 public GameObject foodText;
 public GameObject moraleText;
 public GameObject staminaText;
 public GameObject sailorText;
 
+public bool paused = false;
+
+//how frequently your sailors must eat
 public int framesPerConsuption = 120;
 private int frameIterator = 0;
 
+//rates of food and water consumption
 public float foodPerSailorPerConsumption = 0.005f;
 public float lowRationFoodPerSailorPerConsumption = 0.0025f;
 public float highRationFoodPerSailorPerConsumption = 0.0075f;
@@ -23,6 +28,7 @@ public float highRationWaterPerSailorPerConsumption = 0.015f;
 public int rationRate = 1;
 public int moveRate = 1;
 
+//rates of morale and stamina loss/gain
 public float moraleLossFromLowRations = 0.5f;
 public float moraleGainFromHighRations = 0.5f;
 public float staminaLossFromHighSpeed = 0.5f;
@@ -30,6 +36,7 @@ public float staminaLossFromLowRations= 0.5f;
 public float staminaGainFromLowSpeed = 0.5f;
 public float staminaGainFromHighRations= 0.5f;
 
+//amount of resources
 public float food = 100f;
 public float water = 100f;
 public int sailors = 100;
@@ -51,49 +58,21 @@ public float morale = 100f;
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+	if(paused == false)
+	{
 		if(frameIterator>framesPerConsuption)
 		{
 			frameIterator = 0;
+			Consume(1);
 			
-			if(rationRate == 0)
-			{
-				food = food - (sailors*lowRationFoodPerSailorPerConsumption);
-				water = water - (sailors*lowRationWaterPerSailorPerConsumption);
-				morale = morale - moraleLossFromLowRations;
-				stamina = stamina - staminaLossFromLowRations;
-			}
-			
-			if(rationRate == 1)
-			{
-				food = food - (sailors*foodPerSailorPerConsumption);
-				water = water - (sailors*waterPerSailorPerConsumption);
-			}
-			
-			if(rationRate == 2)
-			{
-				food = food - (sailors*highRationFoodPerSailorPerConsumption);
-				water = water - (sailors*highRationWaterPerSailorPerConsumption);
-				morale = morale + moraleGainFromHighRations;
-				stamina = stamina + staminaGainFromHighRations;
-			}
-			
-			if(moveRate == 0)
-			{
-				stamina = stamina + staminaGainFromLowSpeed;
-			}
-			
-			if(moveRate == 2)
-			{
-				stamina = stamina - staminaLossFromHighSpeed;
-			}
-			
-			waterText.GetComponent<Text>().text = "water: " + water.ToString() + "L";
-			foodText.GetComponent<Text>().text = "food: " + food.ToString() + "Kg";
-			moraleText.GetComponent<Text>().text = "morale: " + morale.ToString() + "%";
-			staminaText.GetComponent<Text>().text = "stamina: " + stamina.ToString() + "%";
-			sailorText.GetComponent<Text>().text = "sailors: " + sailors.ToString();
 		}
 		frameIterator = frameIterator + 1;
+		waterText.GetComponent<Text>().text = "water: " + water.ToString() + "L";
+		foodText.GetComponent<Text>().text = "food: " + food.ToString() + "Kg";
+		moraleText.GetComponent<Text>().text = "morale: " + morale.ToString() + "%";
+		staminaText.GetComponent<Text>().text = "stamina: " + stamina.ToString() + "%";
+		sailorText.GetComponent<Text>().text = "sailors: " + sailors.ToString();
+	}
 	}
 	
 	public void LowRations()
@@ -124,5 +103,54 @@ public float morale = 100f;
 	public void HighSpeed()
 	{
 		moveRate = 2;
+	}
+	
+	public void Consume(int Consumptions)
+	{
+	for(int i = 0;i<Consumptions;i++)
+	{
+		if(rationRate == 0)
+		{
+			food = food - (sailors*lowRationFoodPerSailorPerConsumption);
+			water = water - (sailors*lowRationWaterPerSailorPerConsumption);
+			morale = morale - moraleLossFromLowRations;
+			stamina = stamina - staminaLossFromLowRations;
+		}
+		
+		if(rationRate == 1)
+		{
+			food = food - (sailors*foodPerSailorPerConsumption);
+			water = water - (sailors*waterPerSailorPerConsumption);
+		}
+		
+		if(rationRate == 2)
+		{
+			food = food - (sailors*highRationFoodPerSailorPerConsumption);
+			water = water - (sailors*highRationWaterPerSailorPerConsumption);
+			morale = morale + moraleGainFromHighRations;
+			stamina = stamina + staminaGainFromHighRations;
+		}
+		
+		if(moveRate == 0)
+		{
+			stamina = stamina + staminaGainFromLowSpeed;
+		}
+		
+		if(moveRate == 2)
+		{
+			stamina = stamina - staminaLossFromHighSpeed;
+		}	
+	}
+	
+	}
+	
+	public void Pause()
+	{
+	paused = true;
+	}
+	
+	public void Unpause()
+	{
+		paused = false;
 	}
 }
