c99ff5badaa0a0a9b5d9caf073f9b39a65648aba
DeeCeptor <sirmichaellong@gmail.com>
2015-09-17 17:59:36 -0600

    Implemented splash screen, main menu and credits.

---
+++ Odyssey/Assets/Scripts/MainMenu/Main_Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu : MonoBehaviour 
+{
+    public void Show_Credits()
+    {
+        SceneManager.scene_manager.Start_Conversation(GameObject.Find("SwitchToCredits"));
+    }
+
+
+    public void Start_Game()
+    {
+        SceneManager.scene_manager.Start_Conversation(GameObject.Find("StartGame"));
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+    void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
