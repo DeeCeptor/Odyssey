68b254206f9cf1124de0bb7c55d51e87902c2267
DeeCeptor <sirmichaellong@gmail.com>
2015-10-14 19:37:19 -0600

    no message

--- Odyssey/Assets/Scripts/MoveUI.cs
+++ Odyssey/Assets/Scripts/MoveUI.cs
@@ -4,16 +4,60 @@ using System.Collections;
 public class MoveUI : MonoBehaviour {
     RectTransform rect;
     public Vector3 movement_direction;
+    Vector3 start_pos;
+    Vector3 left_pos = new Vector3(-1500, 0, 0);
 
-	// Use this for initialization
-	void Start () {
+    bool transition_out;
+    bool transition_in;
+    float cur_time;
+    float max_time = 2;
+    public bool finished = false;
+
+
+    void Start () {
         rect = this.GetComponent<RectTransform>();
-        Debug.Log("B");
+        start_pos = rect.localPosition;
+        TransitionOut();
     }
 	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log("A");
-        rect.localPosition = rect.localPosition + movement_direction;
+
+	void Update ()
+    {
+        // Move from the center to the far right
+        if (transition_in && cur_time < max_time)
+        {
+            cur_time += Time.deltaTime;
+            rect.localPosition = rect.localPosition + movement_direction;
+
+            if (cur_time >= max_time)
+                finished = true;
+        }
+        // Move from the far left to the center of the screen
+        else if (transition_out)
+        {
+            rect.localPosition = Vector3.Lerp(left_pos, start_pos, Time.time - cur_time);
+
+            if (rect.localPosition == left_pos)
+                finished = true;
+        }
 	}
+
+
+    // Start out of sight on the left, and move until at center
+    public void TransitionOut()
+    {
+        rect.localPosition = left_pos;
+        transition_out = true;
+        cur_time = Time.time; ;
+        finished = false;
+    }
+
+    // Start at the center, and move to the right
+    public void TransitionIn()
+    {
+        transition_in = true;
+        cur_time = 0;
+        rect.localPosition = start_pos;
+        finished = false;
+    }
 }
