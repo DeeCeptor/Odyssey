3688220fa31f3e1d4d00faf5fcc1bce56987403d
DeeCeptor <sirmichaellong@gmail.com>
2015-10-27 15:18:35 -0600

    no message

--- Odyssey/Assets/Scripts/OverworldScripts/RepeatedlyScaleUp.cs
+++ Odyssey/Assets/Scripts/OverworldScripts/RepeatedlyScaleUp.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RepeatedlyScaleUp : MonoBehaviour {
+public class RepeatedlyScaleUp : MonoBehaviour
+{
+    private Vector3 initial_scale;
+    float expand_speed = 0.5f;
+    float fade_speed = 0.5f;
+    SpriteRenderer sprite;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        initial_scale = this.transform.localScale;
+        sprite = this.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // Constantly increase scale to a factor
+        Vector3 cur_scale = this.transform.localScale;
+        cur_scale.x += Time.deltaTime * expand_speed;
+        cur_scale.y += Time.deltaTime * expand_speed;
+        this.transform.localScale = cur_scale;
+        Debug.Log(transform.localScale);
+
+        // Reset if transparent
+        if (sprite.color.a <= 0)
+        {
+            sprite.color = Color.white;
+            this.transform.localScale = initial_scale;
+        }
+        // Become transparent
+        else if (cur_scale.x > initial_scale.x * 2)
+        {
+            Color c = sprite.color;
+            c.a -= Time.deltaTime * fade_speed;
+            sprite.color = c;
+        }
+    }
 }
